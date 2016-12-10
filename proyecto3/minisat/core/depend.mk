9bf3e5399ffd27e963526af8156a6b088e760bbb
L <luisdev94@gmail.com>
2016-12-09 20:04:40 -0400

    push con el ejecutable

---
+++ proyecto3/minisat/core/depend.mk
@@ -0,0 +1,27 @@
+/home/ozil/IApsvn/proyecto3/minisat/core/Solver.o /home/ozil/IApsvn/proyecto3/minisat/core/Solver.or /home/ozil/IApsvn/proyecto3/minisat/core/Solver.od /home/ozil/IApsvn/proyecto3/minisat/core/Solver.op: /home/ozil/IApsvn/proyecto3/minisat/core/Solver.cc \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Sort.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Vec.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/IntTypes.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/XAlloc.h \
+ /home/ozil/IApsvn/proyecto3/minisat/core/Solver.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Heap.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Alg.h \
+ /home/ozil/IApsvn/proyecto3/minisat/utils/Options.h \
+ /home/ozil/IApsvn/proyecto3/minisat/utils/ParseUtils.h \
+ /home/ozil/IApsvn/proyecto3/minisat/core/SolverTypes.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Map.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Alloc.h
+/home/ozil/IApsvn/proyecto3/minisat/core/Main.o /home/ozil/IApsvn/proyecto3/minisat/core/Main.or /home/ozil/IApsvn/proyecto3/minisat/core/Main.od /home/ozil/IApsvn/proyecto3/minisat/core/Main.op: /home/ozil/IApsvn/proyecto3/minisat/core/Main.cc \
+ /home/ozil/IApsvn/proyecto3/minisat/utils/System.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/IntTypes.h \
+ /home/ozil/IApsvn/proyecto3/minisat/utils/ParseUtils.h \
+ /home/ozil/IApsvn/proyecto3/minisat/utils/Options.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Vec.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/XAlloc.h \
+ /home/ozil/IApsvn/proyecto3/minisat/core/Dimacs.h \
+ /home/ozil/IApsvn/proyecto3/minisat/core/SolverTypes.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Alg.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Map.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Alloc.h \
+ /home/ozil/IApsvn/proyecto3/minisat/core/Solver.h \
+ /home/ozil/IApsvn/proyecto3/minisat/mtl/Heap.h
